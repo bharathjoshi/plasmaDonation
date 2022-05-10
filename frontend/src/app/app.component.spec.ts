597206f33ed8dbc9b32d585c71860643a970776e
aryavrdhn <AryaVardhan.Reddy@iiitb.ac.in>
2022-05-10 18:15:01 +0530

    latest

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ang-fundamentals'`, () => {
+  it(`should have as title 'bloodDonor'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ang-fundamentals');
+    expect(app.title).toEqual('bloodDonor');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ang-fundamentals app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('bloodDonor app is running!');
   });
 });
