a157c3dd9b10b51d2f79464ee006a1b64154acae
Bharath Joshi <bharathjoshi@Bharaths-MacBook-Air.local>
2022-05-10 16:23:09 +0530

    Adding model classes

---
+++ src/main/java/com/application/plasmaDonationApplication.java
@@ -0,0 +1,11 @@
+package com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class plasmaDonationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(plasmaDonationApplication.class, args);
+    }
+}
